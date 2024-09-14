46e33305297a63dc738476eaa7c8b9aeaa90d981
nmdinusth <ducnmba12-050@st.usth.edu.vn>
2024-09-15 00:05:43 +0700

    Sua cac thu lien quan den perspectives, them anh vao drawable, sua androidmanifest ve hien thi bai bao opinion 2,3 perspective 1,2,3

---
+++ NewsApp/app/src/main/java/vn/edu/usth/newsreader/duc_works/ActivityBao1.java
@@ -0,0 +1,16 @@
+package vn.edu.usth.newsreader.duc_works;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.edu.usth.newsreader.R;
+
+public class ActivityBao1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(vn.edu.usth.newsreader.R.layout.activity_bao1);
+    }
+}
