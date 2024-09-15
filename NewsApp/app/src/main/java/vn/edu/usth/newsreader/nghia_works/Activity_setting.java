ed92a2e99169bb488d0e901e697ada3f63bc7e6c
nghia nguyen <nghia@gmail.com>
2024-09-15 13:47:19 +0700

    fix setting_noti

--- NewsApp/app/src/main/java/vn/edu/usth/newsreader/nghia_works/Activity_setting.java
+++ NewsApp/app/src/main/java/vn/edu/usth/newsreader/nghia_works/Activity_setting.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.newsreader.nghia_works;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,10 @@ public class Activity_setting extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_setting_nghia);
 
+    }
+    public void clickNotificationnghia(View view) {
+        // Khởi tạo Intent để mở SecondActivity
+        Intent intent = new Intent(Activity_setting.this, Activity_setting_noti.class);
+        startActivity(intent);
     }
 }
