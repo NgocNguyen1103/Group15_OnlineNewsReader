192beb1ba68be265c642357a89643de62759a3bf
Pham Duc Phuong <phuongpd.ba12-150@st.usth.edu.vn>
2024-09-15 13:53:48 +0700

    Add podcast fragment 12 and onlick

---
+++ NewsApp/app/src/main/java/vn/edu/usth/newsreader/phuong_works/Activity_podcast_2.java
@@ -0,0 +1,26 @@
+package vn.edu.usth.newsreader.phuong_works;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.newsreader.R;
+
+public class Activity_podcast_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_podcast2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
