d3f0ed48d3e75d6dee6fdd5667c47986a6ffa638
Nguyen Quang Ngoc <quangquangngoc@gmail.com>
2024-09-15 15:52:20 +0700

    Delete some unsused functions and comment

--- NewsApp/app/src/main/java/vn/edu/usth/newsreader/phuong_works/Activity_podcast_2.java
+++ NewsApp/app/src/main/java/vn/edu/usth/newsreader/phuong_works/Activity_podcast_2.java
@@ -17,10 +17,6 @@ public class Activity_podcast_2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_podcast2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
