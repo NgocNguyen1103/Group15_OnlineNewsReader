4b0071fb7d408911f8496f1b26748f98132e3fa9
duyusth <duynd.ba12-062@st.usth.edu.vn>
2024-09-15 08:48:53 +0700

    Fix Activity and UI

--- NewsApp/app/src/main/java/vn/edu/usth/newsreader/duy_works/BallDetailActivity.java
+++ NewsApp/app/src/main/java/vn/edu/usth/newsreader/duy_works/BallDetailActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.newsreader.duy_works;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,11 @@ public class BallDetailActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ball_detail);
 
-    }
-}
+        LinearLayout btnSendFeedback = findViewById(R.id.btn_send_feedback);
+        btnSendFeedback.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FeedbackActivity.class);
+            startActivity(intent);
+
+
+        });
+    }}
